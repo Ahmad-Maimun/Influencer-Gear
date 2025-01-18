e7e3085abc817759cffbffe6a7da4b15a479e242
Ahmad-Maimun <ahmadmaimun.wdd@gmail.com>
2025-01-18 15:26:54 +0600

    Navbar Done

---
+++ src/assets/images/images.js
@@ -0,0 +1,3 @@
+import searchIcon from './icons/search.png';
+import cartIcon from './icons/cart.png';
+export { searchIcon, cartIcon };
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,42 @@
 import './Navbar.css';
+import { searchIcon } from '../../assets/images/images';
+import { cartIcon } from '../../assets/images/images';
 const Navbar = () => {
-  return <h1>Navbar</h1>;
+  return (
+    <>
+      <div className="navbar container">
+        <div>
+          <span>Influencer Gear</span>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Products</a>
+            </li>
+            <li>
+              <a href="#">Sign up</a>
+            </li>
+            <li>
+              <a href="#">Log In</a>
+            </li>
+            <li>
+              <a href="#">
+                <img src={searchIcon} alt="" />
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src={cartIcon} alt="" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
