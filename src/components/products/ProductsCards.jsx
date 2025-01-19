f44661aad8b499ea0e2e928ba8d208c44c1f2d64
Ahmad-Maimun <ahmadmaimun.wdd@gmail.com>
2025-01-19 12:09:55 +0600

    Static Product Card Design Done

--- src/components/products/ProductsCards.jsx
+++ src/components/products/ProductsCards.jsx
@@ -5,8 +5,22 @@ import './ProductsCards.css';
 const ProductsCards = () => {
   return (
     <div className="products container">
-      <h3>Popular Collection</h3>
-      <div>
+      <h3 className="products-title">Popular Collection</h3>
+      <div className="products-cards">
+        <div className="product-card">
+          <img src={tripod} alt="" />
+          <div className="product-info">
+            <h4>Flex Tripod</h4>
+            <span>$50.48</span>
+            <strong>
+              <IoMdStar />
+              <span>4.5</span>
+            </strong>
+            <p className="product-desc">
+              Wordwide shifting available Buyers protection possible!
+            </p>
+          </div>
+        </div>
         <div className="product-card">
           <img src={tripod} alt="" />
           <div className="product-info">
@@ -15,6 +29,22 @@ const ProductsCards = () => {
             <strong>
               <IoMdStar />
             </strong>
+            <p className="product-desc">
+              Wordwide shifting available Buyers protection possible!
+            </p>
+          </div>
+        </div>
+        <div className="product-card">
+          <img src={tripod} alt="" />
+          <div className="product-info">
+            <h4>Flex Tripod</h4>
+            <span>$50.48</span>
+            <strong>
+              <IoMdStar />
+            </strong>
+            <p className="product-desc">
+              Wordwide shifting available Buyers protection possible!
+            </p>
           </div>
         </div>
       </div>
